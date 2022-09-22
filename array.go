64e3811317673d62d699abfc0c0ae81cce5142e3
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-23 01:05:25 +0700

    Khai báo không đủ giá trị khởi tạo.

--- array.go
+++ array.go
@@ -16,4 +16,8 @@ func main() {
 	//khai báo và khởi tạo giá trị
 	arrays := [3]int{1, 2, 3}
 	fmt.Println(arrays)
+
+	//khai báo nhưng khởi tạo không đủ giá trị
+	arrays2 := [3]int{100} //100 sẽ được gán vào phần tử đầu tiên
+	fmt.Println(arrays2)   //[100 0 0]
 }
